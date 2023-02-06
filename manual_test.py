818e50d05cbdccaaabada34a740ae2c36f399d78
João Palmeiro <joaommpalmeiro@gmail.com>
2023-02-06 10:10:41 +0000

    Minor tweaks

--- manual_test.py
+++ manual_test.py
@@ -3,6 +3,8 @@
 # https://github.com/microsoft/pylance-release/issues/196#issuecomment-1081448386
 # https://github.com/microsoft/pyright/blob/main/docs/comments.md#file-level-type-controls
 
+# Via terminal: poetry shell + flake8 manual_test.py
+
 import altair
 import numpy
 import pandas
